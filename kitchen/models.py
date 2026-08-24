2dd1482868fdd2e7885172a06bd492615d3626b1
Artem Yur <artemonodon@gmail.com>
2026-08-24 08:52:10 +0200

    Add custom Cook model

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(default=0)

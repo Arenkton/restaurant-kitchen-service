6ffb2a102dc68632d671661f78c1ad49c5d54d26
Artem Yur <artemonodon@gmail.com>
2026-08-24 09:03:00 +0200

    Add DishType and Dish models

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.urls import reverse
 
 
 class Cook(AbstractUser):
     years_of_experience = models.PositiveIntegerField(default=0)
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook)
+
+    def __str__(self):
+        return self.name
